842d5964e8442cf49a7cbaef1c218ba73b0e8f48
Raptor Tech <44273999+coldcape@users.noreply.github.com>
2022-03-24 11:01:43 +0100

    at last

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String[] argv)
-    {
+    public static void main (String[] argv) throws FileNotFoundException {
 
+        binaryTree tree = new binaryTree();
+
+        File file = new File("src/com/company/textInput.txt");
+        Scanner read = new Scanner(file);
+
+        while (read.hasNext()){
+            String linje = read.nextLine();
+            String setning = linje.replaceAll("[^a-zA-Z0-9]", " ");
+            setning = linje.replaceAll("  ", " ");
+
+
+            String[] ord = setning.split(" ");
+            for (String o: ord){
+                tree.leggTilNode(o.toUpperCase());
+            }
+        }
+        tree.inorder();
     }
 }
--- src/com/company/binaryTree.java
+++ src/com/company/binaryTree.java
@@ -1,139 +1,98 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Objects;
-import java.util.Scanner;
+package com.company;
+
 
 public class binaryTree {
+    treNode root;
 
-    private static class textNode{
+    public binaryTree() {
+        this.root = null;
+    }
+
+    private static class treNode {
         String data;
-        textNode left, right;
+        treNode left, right;
 
         public int count = 1;
 
-        public textNode(String data){
+        public treNode(String data) {
             this.data = data;
             left = right = null;
         }
 
-        void write (){
-            System.out.print(data + " x" + count + ", ");
+        // Ikke elegant, men det fungerer...
+        void write()
+        {
+            System.out.print("\n" + this.data + " [" + count + "] ,");
         }
 
-        public void addNode(String data) {
-            if (this.data == null) {
-                this.data = data;
-            } else {
-                if (this.data.compareTo(data) < 0) {
-                    if (this.left != null) {
-                        this.left.addNode(data);
-                    }
-                    else {
-                        this.left = new textNode(data);
-                    }
 
-                } else {
-                    if (this.right != null) {
-                        this.right.addNode(data);
-                    } else {
-                        this.right = new textNode(data);
+    }
+
+
+
+    public boolean tomTree(){
+
+        return root == null;
+    }
+
+    public void leggTilNode(String data){
+
+
+        if (tomTree()){
+            this.root = new treNode(data);
+        }
+        else {
+            treNode current = this.root;
+
+
+            boolean innsetting = true;
+            while (innsetting){
+                int compare = data.compareTo(current.data);
+
+                if (compare == 0){ // Verdien pa strengene er like
+                    current.count++;
+                    innsetting = false;
+                }
+                else if (compare < 0){ // Mindre enn null betyr at stringen kommer alfabetisk for verdien vi sammenligner mer
+                    if (current.left == null) {
+                        current.left = new treNode(data);
                     }
+                    current = current.left;
+                }
+                else {
+                    if (current.right == null) {
+                        current.right = new treNode(data);
+                    }
+                    current = current.right;
 
                 }
             }
         }
 
-        public void inOrder(textNode data){
-            if (data != null){
-                inOrder(data.right);
-                data.write();
-                inOrder(data.left);
+    }
 
-            }
+
+    // Printout of inorder tree traversal, for demo purposes
+    //
+    public void inorder()
+    {
+        System.out.print("Inorder: ");
+        inorder(root);
+        System.out.println("\n");
+    }
+
+    private void inorder(treNode t)
+    {
+        if (t != null)
+        {
+            inorder(t.left);
+            t.write();
+            inorder(t.right);
         }
-
-        boolean contains(textNode root, String x) {
-            if (root == null)
-                return false;
-            if (Objects.equals(root.data, x)) {
-                root.count += 1;
-                return true;
-            }
-
-            return (contains(root.left, x) || contains(root.right, x));
-        }
-
     }
 
-    public static textNode createTree(String data ) {
-        textNode tree = new textNode(data);
-        if( data != null ) {
-
-            data = data.replaceAll("[^a-zA-Z0-9]", " ");
-            String[] words = data.split( " ");
-
-            tree = new textNode(null);
-            for (String word : words) {
-                if (!tree.contains(tree, word)) {
-                    tree.addNode(word);
-                }
-            }
-
-        }
-        return tree;
-    }
-
-
-    public static void mainProgram() throws FileNotFoundException {
-
-        Scanner scan = new Scanner(System.in);
-        File file = new File("textInput.txt");
-        Scanner read = new Scanner(file);
-        String freetxt;
-
-        System.out.println("""
-                 Welcome to text analysis
-                 Choose starting method:
-                 Keyboard input: 1
-                 File input: 2
-                 
-                 WORDS ARE SORTED FROM BOTTOM(LEFT) TO TOP(RIGHT)""");
-
-        int ans = scan.nextInt();
-        scan.nextLine();
-
-        switch (ans){
-
-            case 1 -> {
-                System.out.println("Enter your free text");
-                freetxt = scan.nextLine();
-                keyInput(freetxt.toUpperCase());
-            }
-
-            case 2 -> {
-                while(read.hasNextLine()){
-                    freetxt = read.nextLine();
-                    keyInput(freetxt.toUpperCase());
-                }
-
-            }
-        }
-
-    }
-
-    public static void keyInput(String txt) {
-        textNode tester = createTree(txt.toUpperCase());
-        System.out.println("\nIN ORDER: ");
-        tester.inOrder(tester);
-
-    }
-
-
-
-
-
-    public static void main(String[] args) throws FileNotFoundException {
-        mainProgram();
-    }
 
 }
+
+
+
